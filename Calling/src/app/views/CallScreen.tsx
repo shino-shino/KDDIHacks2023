e31133ea87924fc5d1e7c336209fb6487eaab532
Miyamura <75057367+MiyamuP@users.noreply.github.com>
2023-07-09 03:53:45 +0900

    Update CallScreen.tsx
    画像切り替え

--- Calling/src/app/views/CallScreen.tsx
+++ Calling/src/app/views/CallScreen.tsx
@@ -12,7 +12,7 @@ import {
   toFlatCommunicationIdentifier,
 } from '@azure/communication-react';
 
-import React, { useCallback, useMemo, useRef } from 'react';
+import React, { useCallback, useMemo, useRef, useEffect, useState, createContext, useContext } from 'react';
 import { createAutoRefreshingCredential } from '../utils/credential';
 import { WEB_APP_TITLE } from '../utils/AppUtils';
 import { CallCompositeContainer } from './CallCompositeContainer';
@@ -28,10 +28,57 @@ export interface CallScreenProps {
   displayName: string;
 }
 
+const FaceContext = createContext({
+  faceType: 'normal',
+  changeFaceType: (n: string) => { }
+});
+
 export const CallScreen = (props: CallScreenProps): JSX.Element => {
   const { token, userId } = props;
   const callIdRef = useRef<string>();
+  const pressInterval = 10;
 
+  const [faceType, setFaceType] = useState<string>('normal');
+
+  const changeFaceType = (s: string): void => {
+    setFaceType(s);
+  };
+
+  // Key Down
+
+  const keyFunction = useCallback((event) => {
+    switch (event.keyCode) {
+      case 65:
+        setTimeout(() => {
+          console.log("A is pressed!");
+          changeFaceType('normal');
+        }, pressInterval);
+
+        break;
+      case 66:
+        setTimeout(() => {
+          console.log("B is pressed!");
+          changeFaceType('positive');
+        }, pressInterval);
+        break;
+      case 67:
+        setTimeout(() => {
+          console.log("C is pressed!");
+          changeFaceType('negative');
+        }, pressInterval);
+        break;
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyFunction, false);
+    return () => {
+      document.removeEventListener("keydown", keyFunction, false);
+    }
+  }, [keyFunction]);
+
+
+  // Azure
   const subscribeAdapterEvents = useCallback((adapter: CommonCallAdapter) => {
     adapter.on('error', (e) => {
       // Error is already acted upon by the Call composite, but the surrounding application could
@@ -61,7 +108,11 @@ export const CallScreen = (props: CallScreenProps): JSX.Element => {
     return createAutoRefreshingCredential(toFlatCommunicationIdentifier(userId), token);
   }, [token, userId]);
 
-  return <AzureCommunicationCallScreen afterCreate={afterCallAdapterCreate} credential={credential} {...props} />;
+  return (
+    <FaceContext.Provider value={{ faceType, changeFaceType }}>
+      <AzureCommunicationCallScreen afterCreate={afterCallAdapterCreate} credential={credential} {...props} />
+    </FaceContext.Provider>
+  );
 };
 
 type AzureCommunicationCallScreenProps = CallScreenProps & {
@@ -85,6 +136,329 @@ const AzureCommunicationCallScreen = (props: AzureCommunicationCallScreenProps):
     afterCreate
   );
 
+  // 伊藤切り替え
+  const onRenderPlaceholder_Ito = () => {
+    switch (useContext(FaceContext).faceType) {
+      case 'normal':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/sJ0Fm5N.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      case 'positive':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/dpBrZGX.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+      case 'negative':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/nZsd3vM.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      default:
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/sJ0Fm5N.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+    }
+
+  }
+
+  // 城石切り替え
+  const onRenderPlaceholder_Shiroishi = () => {
+    switch (useContext(FaceContext).faceType) {
+      case 'normal':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/oXyrTui.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      case 'positive':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/V7AV0k4.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+      case 'negative':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/E4wzzid.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      default:
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/oXyrTui.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+    }
+
+  }
+
+  // 篠原切り替え
+  const onRenderPlaceholder_Shinohara = () => {
+    switch (useContext(FaceContext).faceType) {
+      case 'normal':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/wNCKJJS.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      case 'positive':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/YzbpWfN.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+      case 'negative':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/1Jk2eib.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      default:
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/wNCKJJS.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+    }
+
+  }
+
+  // 宮村切り替え
+  const onRenderPlaceholder_Miyamura = () => {
+    switch (useContext(FaceContext).faceType) {
+      case 'normal':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/bY1wqNC.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      case 'positive':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/VYO6ij3.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+      case 'negative':
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/Vezm74Y.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+      default:
+        return (
+          <Stack>
+            <img
+              src="https://i.imgur.com/bY1wqNC.png"
+              style={{
+                borderRadius: '170px',
+                width: '170px',
+                position: 'absolute',
+                margin: 'auto',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0
+              }}
+            />
+          </Stack>
+        );
+
+    }
+
+  }
   // 犬gif用
   // const onRenderPlaceholder = (): JSX.Element => (
   //   <Stack>
@@ -124,23 +498,23 @@ const AzureCommunicationCallScreen = (props: AzureCommunicationCallScreenProps):
   // );
 
   // ito_normal用
-  const onRenderPlaceholder_ito_normal = (): JSX.Element => (
-    <Stack>
-      <img
-        src="https://i.imgur.com/sJ0Fm5N.png"
-        style={{
-          borderRadius: '170px',
-          width: '170px',
-          position: 'absolute',
-          margin: 'auto',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0
-        }}
-      />
-    </Stack>
-  );
+  // const onRenderPlaceholder_ito_normal = (): JSX.Element => (
+  //   <Stack>
+  //     <img
+  //       src="https://i.imgur.com/sJ0Fm5N.png"
+  //       style={{
+  //         borderRadius: '170px',
+  //         width: '170px',
+  //         position: 'absolute',
+  //         margin: 'auto',
+  //         left: 0,
+  //         right: 0,
+  //         top: 0,
+  //         bottom: 0
+  //       }}
+  //     />
+  //   </Stack>
+  // );
 
   // ito_positive用
   // const onRenderPlaceholder_ito_positive = (): JSX.Element => (
@@ -334,98 +708,98 @@ const AzureCommunicationCallScreen = (props: AzureCommunicationCallScreenProps):
 
   // const videoTileStyles = { root: { height: '300px', width: '400px', border: '1px solid #999' } };
   const videoTileStyles = { root: { height: '205px', width: '500px', border: '1px solid #999' } };
-  
+
   return (
-  <>
-    <CallCompositeContainer {...props} adapter={adapter} />
-    <FluentThemeProvider>
-    
-    <div style={{ marginTop: '0px', display: 'flex', alignItems: 'center' }}>
-      <VideoTile
-        userId="UserIdPlaceholder"
-        styles={videoTileStyles}
-        displayName={'Iさん'}
-        renderElement={null}
-        isMirrored={true}
-        onRenderPlaceholder={onRenderPlaceholder_ito_normal}
-      />
-      
-      <img src="https://i.imgur.com/KZILTky.png" style={{
-        height: '200px', width: '200px'
-      }} />
-      {/* <img src="https://i.imgur.com/9IaSKbt.png" style={{
-        height: '200px', width: '200px'
-      }} /> */}
-      {/* <img src="https://i.imgur.com/EbOS6Fs.png" style={{
-        height: '200px', width: '200px'
-      }} /> */}
-    </div>
+    <>
+      <CallCompositeContainer {...props} adapter={adapter} />
+      <FluentThemeProvider>
 
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <VideoTile
-        userId="UserIdPlaceholder"
-        styles={videoTileStyles}
-        displayName={'Sさん'}
-        renderElement={null}
-        isMirrored={true}
-        onRenderPlaceholder={onRenderPlaceholder_shinohara_normal}
-      />
+        <div style={{ marginTop: '0px', display: 'flex', alignItems: 'center' }}>
+          <VideoTile
+            userId="UserIdPlaceholder"
+            styles={videoTileStyles}
+            displayName={'Iさん'}
+            renderElement={null}
+            isMirrored={true}
+            onRenderPlaceholder={onRenderPlaceholder_Ito}
+          />
 
-      {/* <img src="https://i.imgur.com/KZILTky.png" style={{
+          <img src="https://i.imgur.com/KZILTky.png" style={{
+            height: '200px', width: '200px'
+          }} />
+          {/* <img src="https://i.imgur.com/9IaSKbt.png" style={{
         height: '200px', width: '200px'
       }} /> */}
-      <img src="https://i.imgur.com/9IaSKbt.png" style={{
-        height: '200px', width: '200px'
-      }} />
-      {/* <img src="https://i.imgur.com/EbOS6Fs.png" style={{
+          {/* <img src="https://i.imgur.com/EbOS6Fs.png" style={{
         height: '200px', width: '200px'
       }} /> */}
-    </div>
+        </div>
 
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <VideoTile
-        userId="UserIdPlaceholder"
-        styles={videoTileStyles}
-        displayName={'Sさん'}
-        renderElement={null}
-        isMirrored={true}
-        onRenderPlaceholder={onRenderPlaceholder_shiroishi_normal}
-      />
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <VideoTile
+            userId="UserIdPlaceholder"
+            styles={videoTileStyles}
+            displayName={'Sさん'}
+            renderElement={null}
+            isMirrored={true}
+            onRenderPlaceholder={onRenderPlaceholder_Shinohara}
+          />
 
-      {/* <img src="https://i.imgur.com/KZILTky.png" style={{
+          {/* <img src="https://i.imgur.com/KZILTky.png" style={{
         height: '200px', width: '200px'
       }} /> */}
-      {/* <img src="https://i.imgur.com/9IaSKbt.png" style={{
+          <img src="https://i.imgur.com/9IaSKbt.png" style={{
+            height: '200px', width: '200px'
+          }} />
+          {/* <img src="https://i.imgur.com/EbOS6Fs.png" style={{
         height: '200px', width: '200px'
       }} /> */}
-      <img src="https://i.imgur.com/EbOS6Fs.png" style={{
-        height: '200px', width: '200px'
-      }} />
-    </div>
-    
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <VideoTile
-        userId="UserIdPlaceholder"
-        styles={videoTileStyles}
-        displayName={'Mさん'}
-        renderElement={null}
-        isMirrored={true}
-        onRenderPlaceholder={onRenderPlaceholder_miyamura_normal}
-      />
+        </div>
 
-      <img src="https://i.imgur.com/KZILTky.png" style={{
-        height: '200px', width: '200px'
-      }} />
-      {/* <img src="https://i.imgur.com/9IaSKbt.png" style={{
-        height: '200px', width: '200px'
-      }} /> */}
-      {/* <img src="https://i.imgur.com/EbOS6Fs.png" style={{
-        height: '200px', width: '200px'
-      }} /> */}
-    </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <VideoTile
+            userId="UserIdPlaceholder"
+            styles={videoTileStyles}
+            displayName={'Sさん'}
+            renderElement={null}
+            isMirrored={true}
+            onRenderPlaceholder={onRenderPlaceholder_Shiroishi}
+          />
 
-    </FluentThemeProvider>  
-  </>
+          {/* <img src="https://i.imgur.com/KZILTky.png" style={{
+        height: '200px', width: '200px'
+      }} /> */}
+          {/* <img src="https://i.imgur.com/9IaSKbt.png" style={{
+        height: '200px', width: '200px'
+      }} /> */}
+          <img src="https://i.imgur.com/EbOS6Fs.png" style={{
+            height: '200px', width: '200px'
+          }} />
+        </div>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <VideoTile
+            userId="UserIdPlaceholder"
+            styles={videoTileStyles}
+            displayName={'Mさん'}
+            renderElement={null}
+            isMirrored={true}
+            onRenderPlaceholder={onRenderPlaceholder_Miyamura}
+          />
+
+          <img src="https://i.imgur.com/KZILTky.png" style={{
+            height: '200px', width: '200px'
+          }} />
+          {/* <img src="https://i.imgur.com/9IaSKbt.png" style={{
+        height: '200px', width: '200px'
+      }} /> */}
+          {/* <img src="https://i.imgur.com/EbOS6Fs.png" style={{
+        height: '200px', width: '200px'
+      }} /> */}
+        </div>
+
+      </FluentThemeProvider>
+    </>
   )
 };
 
